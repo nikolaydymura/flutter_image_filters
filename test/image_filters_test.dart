c576e152f4835b0560c99781b71339a41b25e148
Nikolay Dymura <nikolay.dymura@gmail.com>
2022-11-22 01:20:08 +0200

    Added GitHub Actions
    - updated description

--- test/image_filters_test.dart
+++ test/image_filters_test.dart
@@ -1,5 +1,9 @@
+import 'package:flutter_image_filters/flutter_image_filters.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('adds one to input values', () {});
+  test('verify available filters amount', () {
+    final filters = FlutterImageFilters.availableFilters;
+    expect(filters.length, 29);
+  });
 }
