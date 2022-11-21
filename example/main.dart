685b71b6325f2b05789453f8f1c0ef264ac73e55
Nikolay Dymura <nikolay.dymura@gmail.com>
2022-11-22 01:41:51 +0200

    Added GitHub Actions
    - fixed score

---
+++ example/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_image_filters/flutter_image_filters.dart';
+
+class PreviewPage extends StatefulWidget {
+  const PreviewPage({Key? key}) : super(key: key);
+
+  @override
+  State<PreviewPage> createState() => _PreviewPageState();
+}
+
+class _PreviewPageState extends State<PreviewPage> {
+  late TextureSource texture;
+  late BrightnessShaderConfiguration configuration;
+  bool textureLoaded = false;
+
+  @override
+  void initState() {
+    super.initState();
+    configuration = BrightnessShaderConfiguration();
+    configuration.brightness = 0.5;
+    TextureSource.fromAsset('demo.jpeg')
+        .then((value) => texture = value)
+        .whenComplete(
+          () => setState(() {
+        textureLoaded = true;
+      }),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return textureLoaded
+        ? ImageShaderPreview(
+      texture: texture,
+      configuration: configuration,
+    )
+        : const Offstage();
+  }
+}
