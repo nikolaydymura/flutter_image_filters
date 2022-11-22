5df041d41f1155a5520566fe6a24764d35791729
Nikolay Dymura <nikolay.dymura@gmail.com>
2022-11-22 18:22:35 +0200

    Added demo export tests

---
+++ test/demo_export_all_test.dart
@@ -0,0 +1,58 @@
+import 'dart:io';
+import 'dart:ui';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter_image_filters/flutter_image_filters.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:image/image.dart' as img;
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+  final inputFile = File('test/demo.jpeg');
+
+  late final TextureSource texture;
+  setUpAll(() async {
+    texture = await TextureSource.fromFile(inputFile);
+  });
+
+  test('export all', skip: true, () async {
+
+    for (final name in FlutterImageFilters.availableFilters) {
+      final configuration = FlutterImageFilters.createFilter(displayName: name);
+      if (configuration == null) {
+        continue;
+      }
+      if (configuration is LookupTableShaderConfiguration) {
+        configuration.size = 8;
+        configuration.rows = 8;
+        configuration.columns = 8;
+        await configuration.setLutFile(File('demos/lookup_amatorka.png'));
+      }
+      final output = File('demos/$name.jpg');
+      final size =
+          Size(texture.width.toDouble(), texture.height.toDouble()) / 3;
+      final watch = Stopwatch();
+      watch.start();
+      final image = await configuration.export(texture, size);
+      final bytes = await image.toByteData();
+      debugPrint(
+        'Exporting image took ${watch.elapsedMilliseconds} milliseconds',
+      );
+      if (bytes == null) {
+        continue;
+      }
+      final persistedImage = img.Image.fromBytes(
+        image.width,
+        image.height,
+        bytes.buffer.asUint8List(),
+      );
+      img.JpegEncoder encoder = img.JpegEncoder();
+      final data = encoder.encodeImage(persistedImage);
+      await output.writeAsBytes(data);
+      debugPrint(
+        'Exporting file took ${watch.elapsedMilliseconds} milliseconds',
+      );
+      debugPrint('Exported: ${output.absolute}');
+    }
+  });
+}
