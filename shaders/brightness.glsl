8bee7a2bb78593a034ef4aa0a637a1e9393d6cff
Nikolay Dymura <nikolay.dymura@gmail.com>
2023-01-14 13:25:54 +0200

    modified shader sources to recompile

--- shaders/brightness.glsl
+++ shaders/brightness.glsl
@@ -6,9 +6,13 @@ layout(location = 2) uniform lowp sampler2D inputImageTexture;
 
 layout(location = 0) out vec4 fragColor;
 
+vec4 processColor(vec4 sourceColor){
+    return vec4((sourceColor.rgb + vec3(inputBrightness)), sourceColor.w);
+}
+
 void main() {
     vec2 textureCoordinate = gl_FragCoord.xy / screenSize;
     vec4 textureColor = texture(inputImageTexture, textureCoordinate);
 
-    fragColor = vec4((textureColor.rgb + vec3(inputBrightness)), textureColor.w);
+    fragColor = processColor(textureColor);
 }
--- shaders/contrast.glsl
+++ shaders/contrast.glsl
@@ -8,9 +8,13 @@ layout(location = 2) uniform sampler2D inputImageTexture;
 layout(location = 0) uniform lowp float inputContrast;
 layout(location = 1) uniform vec2 screenSize;
 
+vec4 processColor(vec4 sourceColor){
+    return vec4(((sourceColor.rgb - vec3(0.5)) * inputContrast + vec3(0.5)), sourceColor.w);
+}
+
 void main() {
     vec2 textureCoordinate = gl_FragCoord.xy / screenSize;
     lowp vec4 textureColor = texture(inputImageTexture, textureCoordinate);
 
-    fragColor = vec4(((textureColor.rgb - vec3(0.5)) * inputContrast + vec3(0.5)), textureColor.w);
+    fragColor = processColor(textureColor);
 }
