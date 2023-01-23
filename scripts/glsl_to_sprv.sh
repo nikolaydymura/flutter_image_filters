c6890a743619bdf6c0081302793b96002d1e8205
Nikolay Dymura <nikolay.dymura@gmail.com>
2023-01-23 16:27:18 +0200

    Added tag for release

--- scripts/glsl_to_sprv.sh
+++ scripts/glsl_to_sprv.sh
@@ -1,2 +1,2 @@
-shader --use-local $HOME/Downloads/install/ --path shaders/ --to-dart
+flutter pub run shader --use-local $HOME/Downloads/install/ --path shaders/ --to-dart
 mv shaders/*.dart lib/src/shaders/
