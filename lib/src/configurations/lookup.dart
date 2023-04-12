1491c81347bd546e073c5f1f4517576844d45ea5
egorterekhov <asya.wear@gmail.com>
2023-04-12 14:34:53 -0700

    Changed documentation in LookupTable

--- lib/src/configurations/lookup.dart
+++ lib/src/configurations/lookup.dart
@@ -53,6 +53,7 @@ class SquareLookupTableShaderConfiguration extends ShaderConfiguration {
   List<ConfigurationParameter> get parameters => [_intensity, _cubeData];
 }
 
+/// Describes HALD lookup table manipulations
 class HALDLookupTableShaderConfiguration extends ShaderConfiguration {
   final RangeNumberParameter _intensity;
   final DataParameter _cubeData;
@@ -69,6 +70,7 @@ class HALDLookupTableShaderConfiguration extends ShaderConfiguration {
         _cubeData = ShaderTextureParameter('inputTextureCubeData', 'HALD LUT'),
         super([1.0]);
 
+  /// Update [_cubeData] value by [setLutImage].
   Future<void> setLutImage(Uint8List value) async {
     _cubeData.data = value;
     _cubeData.asset = null;
@@ -76,6 +78,7 @@ class HALDLookupTableShaderConfiguration extends ShaderConfiguration {
     await _cubeData.update(this);
   }
 
+  /// Update [_cubeData] value by [setLutAsset].
   Future<void> setLutAsset(String value) async {
     _cubeData.data = null;
     _cubeData.asset = value;
@@ -83,6 +86,7 @@ class HALDLookupTableShaderConfiguration extends ShaderConfiguration {
     await _cubeData.update(this);
   }
 
+  /// Update [_cubeData] value by [setLutFile].
   Future<void> setLutFile(File value) async {
     _cubeData.data = null;
     _cubeData.asset = null;
@@ -90,6 +94,9 @@ class HALDLookupTableShaderConfiguration extends ShaderConfiguration {
     await _cubeData.update(this);
   }
 
+  /// Updates the [intensity] value.
+  ///
+  /// The [value] must be in 0.0 and 1.0 range.
   set intensity(double value) {
     _intensity.value = value;
     _intensity.update(this);
